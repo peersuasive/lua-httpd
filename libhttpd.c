a08bbece03aee422f29c3d9f21958239e733384d
steve <devnull@localhost>
2005-10-30 01:02:49 +0000

    2005-10-30 01:02:49 by steve   GPL -> LGPL

--- libhttpd.c
+++ libhttpd.c
@@ -5,16 +5,15 @@
  *   Simple network primitives for use by the Lua 5.0 scripting engine.
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * it under the terms of the GNU Lesser General  Public License (LGPL)
+ *  as published by the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
  *
@@ -51,7 +50,7 @@
 #include <unistd.h>
  
 #define MYNAME		"libhttpd"
-#define VERSION	        "$Id: libhttpd.c,v 1.13 2005-10-29 20:12:01 steve Exp $"
+#define VERSION	        "$Id: libhttpd.c,v 1.14 2005-10-30 01:02:49 steve Exp $"
 
 
 
